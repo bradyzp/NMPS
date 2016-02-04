67694686cd774f98012ddb94292483cc7ec71df2
Zac B <bradyzp@gmail.com>
2016-02-04 09:17:45 +0100

    Renamed Get-Movie
    Changed name and created module instead of ps1.
    Added .gitignore to skip csv files for now.

--- NMPS-Tools.psm1
+++ NMPS-Tools.psm1
@@ -1,5 +1,7 @@
+<#
+
+#>
 function Get-Movie {
-    [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true, Position=0, ParameterSetName='Title')]
         [String]$Title,
@@ -10,40 +12,35 @@ function Get-Movie {
         [Switch]$Tomatoes        
     )
     if($Title) {
-        $request = "t=$Title&type=$Type&plot=$Plot&r=json&tomatoes=$Tomatoes"
+        $request = "?t=$Title&type=$Type&plot=$Plot&r=json&tomatoes=$Tomatoes"
         Write-Verbose $request
     } elseif ($imdbID) {
-        $request = "i=$imdbID&type=$Type&plot=$Plot&r=json&tomatoes=$Tomatoes"
+        $request = "?i=$imdbID&type=$Type&plot=$Plot&r=json&tomatoes=$Tomatoes"
         Write-Verbose $request
     }
 
-    $web_result = Invoke-WebRequest -Uri "http://www.omdbapi.com/?$request"
+    $uri = "http://www.omdbapi.com"
+    $web_result = Invoke-WebRequest -Uri "$uri/$request"
     
+    #Encoutered an error performing a title lookup, try to search with API
+    if($web_result.Error -and $Title) {
+        $request = "?s=$Title&type=movie&r=json"
+        Write-Verbose $request
+        $web_result = Invoke-WebRequest -Uri "$uri/$request"
+    }
     ConvertFrom-Json -InputObject $web_result.Content
 }
 
-function Search-Movie {
-    param (
-        [Parameter(Mandatory=$true)]
-        [String]$Title
-    )
 
-    $request = "s=$Title&type=movie&r=json"
-    Write-Verbose $request
+<#
 
-    $web_result = Invoke-WebRequest -Uri "http://www.omdbapi.com/?$request"
-    
-    ConvertFrom-Json -InputObject $web_result.Content
-    
-
-}
-
+#>
 function Generate-NMPSInventory {
     param (
         [Parameter(Mandatory=$true)]
         [String]$src,
         [Int]$limit = -1,
-        [Switch]$reprocess = $false
+        [Switch]$Reprocess = $false
     )
     Write-Verbose $src
     $source_csv = Import-Csv -Path $src
@@ -53,6 +50,9 @@ function Generate-NMPSInventory {
     #for testing
     $counter = 0
     foreach($row in $source_csv) {
+        #####
+        #TESTING BLOCK
+        #####
         if($counter -eq $limit) {
             Write-Warning ("Limit reached - halting processing")
             break
@@ -61,35 +61,37 @@ function Generate-NMPSInventory {
             #Write status message every 50 records
             Write-Warning ("Processing Record $counter")
         }
-        $title = $row.Title
-        $proc = 'true'
 
-        if(-Not $reprocess -and $row.PROCESSED -eq 'true')
-        {
-            #Assume we have already processed this title and skip the lookup
-            $output += $row
-            continue
-            
-            #$metadata = $row
+        ######
+        #Actual Logic
+        ######
+        $Title     = $row.Title
+        $Processed = 'true'
+
+        #Check if this row has already been processed (and reprocess flag isn't set)
+        if(-Not $Reprocess -and ($row.PROCESSED -eq 'true')) {
+            $metadata = $row
+            #continue
         }
-        elseif ($row.imdbID) {
-            #Check for a manually entered imdbID (for manual fix of bad titles)
+        #Check for a manually entered imdb ID# and perform a lookup
+        elseif ($row.imdbID -and ($row.PROCESSED -ne 'true')) {
             Write-Verbose ("Performing imdbID search for $title")
             $metadata = Get-Movie -imdbID $row.imdbID
         }
+        #Else we assume this is a new entry, perform a regular lookup
         else {
-            #Else this is a brand new entered/unfixed movie Title
-            #Fetch movie data from omdbapi.com by Title search
-            $metadata = Get-Movie -Title $title
+            $metadata = Get-Movie -Title $Title
         }
 
         if($metadata.Error) {
-            $proc = 'Lookup Error'
+            $Processed = 'Lookup Error'
             Write-Warning ("Error looking up $title")
         }
         elseif($row.Title -ne $metadata.Title) {
             $warn_title = $metadata.Title
             Write-Verbose ("Title mismatch $Title (original) >> $warn_title (lookup)") 
+            #TODO: Write relevent info out to an error.log
+
             $title = $metadata.Title
         }
      
@@ -97,19 +99,19 @@ function Generate-NMPSInventory {
             "ID" = $row.ID;
             "imdbID" = $metadata.imdbID;
             "IMDB Link" = '';
-            "TITLE" = $title;
+            "TITLE" = $Title;
             "RATED" = $metadata.Rated;
             "EXPIRATION" = '="' + $row.EXPIRATION + '"'; #Preserve the NMPS Expiration code
             "IMDB RATING" = $metadata.imdbRating;
             "GENRE" = $metadata.Genre;
             "PLOT" = $metadata.Plot;
-            "PROCESSED" = $proc
+            "PROCESSED" = $Processed
         }
         $output += New-Object -TypeName psobject -Property $properties
         $counter++
     }
-    #Push it to the pipeline
     $output
 }
 
+Export-ModuleMember -Function 'Generate-NMPSInventory'
 #Generate-NMPSInventory -Input 'C:\Dev\Powershell\NMPSDecember2015Inventory.csv' -Verbose | Export-Csv -Path 'C:\Dev\Powershell\test_export.csv' -Force -NoTypeInformation
